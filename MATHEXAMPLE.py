4c250f5f2befc76de1a3f113f95e61c8648eae1a
Jay Liu <jay.liu@Y22-Design-Jay-Liu.local>
2018-09-17 13:53:58 -0400

    MathExample Update

--- MATHEXAMPLE.py
+++ MATHEXAMPLE.py
@@ -1,2 +1,8 @@
-x=1+2
+x=1+2 #this creats a variable called x and puts 1 + 2 in it py
+print(x)
+#These are comments in Java. When you program
+#And I look at your commments, I always check your 
+#comments 
+# <something = <statement> 
+x = x + 1 
 print(x)
