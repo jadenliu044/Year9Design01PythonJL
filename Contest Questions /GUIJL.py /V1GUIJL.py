a9a52b41c9f9fd7e8597e9626ae85bf0c0473f99
Jay Liu <jay.liu@Y22-Design-Jay-Liu.local>
2018-11-23 12:14:40 -0500

    V1GUIJL.py

--- Contest Questions /GUIJL.py /V1GUIJL.py
+++ Contest Questions /GUIJL.py /V1GUIJL.py
@@ -2,14 +2,22 @@ import tkinter as tk
 
 root = tk.Tk()
 
-label = tk.Label(root, text = "Goal of the Week")
+
+label = tk.Label(root, text = "Goal of the Week:")
 label.config(width = 20, height = 5)
 label.grid(row = 0, column = 0, sticky = "E")
 
 btn1 = tk.Button(root, text = "Calories Consumed")
-btn1.config(width = 5, height = 5)
+btn1.config(width = 10, height = 5)
 btn1.grid(row = 1, column = 0, sticky = "NESW")
 
-btn2 = tk.Button(root, text = )
+btn2 = tk.Button(root, text = "Time Slept")
+btn2.config(width = 10, height = 5)
+btn2.grid(row = 1, column = 1, sticky = "NESW")
+
+btn3 = tk.Button(root, text = "Time per min/kilomter")
+btn3.config(width = 15, height = 5)
+btn3.grid(row = 2, column = 0, sticky = "E")
+
 
 root.mainloop()
