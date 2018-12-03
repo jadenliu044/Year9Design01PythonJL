53699f72be3d7a09a903e6f2b5c8a3989600fd92
Jay Liu <jay.liu@Y22-Design-Jay-Liu.local>
2018-12-03 15:21:33 -0500

    V1GUIJL.py

--- Contest Questions /GUIJL.py /V1GUIJL.py
+++ Contest Questions /GUIJL.py /V1GUIJL.py
@@ -2,22 +2,39 @@ import tkinter as tk
 
 root = tk.Tk()
 
+label = tk.Label(root, text = "Running Fun",)
+label.config(width = 20, height = 10,)
+label.grid(row = 0, column = 0, sticky = "w")
+
 
 label = tk.Label(root, text = "Goal of the Week:")
 label.config(width = 20, height = 5)
-label.grid(row = 0, column = 0, sticky = "E")
+label.grid(row = 1, column = 0, sticky = "NESW")
 
-btn1 = tk.Button(root, text = "Calories Consumed")
-btn1.config(width = 10, height = 5)
-btn1.grid(row = 1, column = 0, sticky = "NESW")
+Text1 = tk.Entry(root)
+Text1.grid(row = 1, column = 1)
 
-btn2 = tk.Button(root, text = "Time Slept")
-btn2.config(width = 10, height = 5)
-btn2.grid(row = 1, column = 1, sticky = "NESW")
+Text1 = tk.Entry(root)
+Text1.grid(row = 2, column = 1,)
 
-btn3 = tk.Button(root, text = "Time per min/kilomter")
-btn3.config(width = 15, height = 5)
-btn3.grid(row = 2, column = 0, sticky = "E")
+Text1 = tk.Entry(root)
+Text1.grid(row = 3, column = 1,)
 
+Text1 = tk.Entry(root)
+Text1.grid(row = 4, column = 1,)
+
+
+label = tk.Label(root, text = "Calories Consumed")
+label.config(width = 10, height = 3)
+label.grid(row = 2, column = 0, sticky = "NESW")
+
+label = tk.Label(root, text = "Time Slept")
+label.config(width = 10, height = 3)
+label.grid(row = 3, column = 0, sticky = "NESW")
+
+label = tk.Label(root, text = "Time per min/kilomter")
+label.config(width = 20, height = 3)
+label.grid(row = 4, column = 0, sticky = "NESW")
 
 root.mainloop()
+
